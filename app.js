8855799ba80e6c0dd7f3e863833f4eaa44243a69
saifalishah123 <123595506+saifalishah123@users.noreply.github.com>
2023-05-09 18:52:14 +0500

    Add files via upload
    hii

---
+++ app.js
@@ -0,0 +1,26 @@
+var btns = document.querySelectorAll("button");
+var display = document.querySelector("#display");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let value = this.innerHTML;
+        if (value === "AC") {
+            display.value = "";
+        } else if (value === "x") {
+            value = "*"
+            display.value += value
+        }
+         else if (value === "÷") {
+            value = "/"
+            display.value += value
+        }
+        else if (value === "=") {
+            display.value = eval(display.value);
+        }
+        else if (value === "CE") {
+            display.value = display.value.toString().slice(0, -1);
+        }
+        else {
+            display.value += value;
+        }
+    })
+}
